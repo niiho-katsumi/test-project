d3f8382accbff31a7da8fceeedea05e57c21649d
niiho-katsumi <amanojaku.ktm@gmail.com>
2026-05-05 09:45:54 +0900

    ArticleListコンポーネント作成

---
+++ src/components/organisms/ArticleList/ArticleList.tsx
@@ -0,0 +1,24 @@
+import "../../../index.css";
+import type { PostInfo } from "../../../type";
+import Article from "../../molecules/Article/Article";
+
+type Props = {
+  articles: PostInfo[];
+};
+
+export default function ArticleList({ articles }: Props) {
+  return (
+    <ul>
+      {articles.map((article) => (
+        <Article PostInfo={{
+              id: article.id,
+              title: article.title,
+              thumbnailUrl: article.thumbnailUrl,
+              createdAt: article.createdAt,
+              categories: article.categories,
+              content: article.content
+          }} />
+      ))}
+    </ul>
+  );
+}
