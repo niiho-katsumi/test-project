0ef145b9e8c5f9f3838ab1d01fad6b17a21c880f
niiho-katsumi <amanojaku.ktm@gmail.com>
2026-05-05 09:42:07 +0900

    TagListコンポーネント作成

---
+++ src/components/molecules/TagList/TagList.tsx
@@ -0,0 +1,16 @@
+import "../../../index.css";
+import Tag from "../../atoms/Tag/Tag";
+
+type Props = {
+  categories: string[];
+};
+
+export default function TagList({ categories }: Props) {
+  return (
+    <div className="flex flex-wrap">
+      {categories.map((category) => (
+        <Tag key={crypto.randomUUID()} category={category} />
+      ))}
+    </div>
+  );
+}
