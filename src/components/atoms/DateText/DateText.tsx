970ea2aa3fd9fa189b074de9f4f61af9ff675665
niiho-katsumi <amanojaku.ktm@gmail.com>
2026-05-05 16:22:23 +0900

    不要なFragment削除

--- src/components/atoms/DateText/DateText.tsx
+++ src/components/atoms/DateText/DateText.tsx
@@ -10,8 +10,6 @@ export default function DateText({ createdAt }: Props) {
   const month = ("00" + (d.getMonth() + 1)).slice(-2);
   const date = ("00" + d.getDate()).slice(-2);
   return (
-    <>
-      <div className="text-[0.8rem] text-[#888]">{year + month + "/" + date}</div>
-    </>
+    <div className="text-[0.8rem] text-[#888]">{year + month + "/" + date}</div>
   );
 }
