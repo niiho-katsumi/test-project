a7287dcd07a3b4d547754ac7bd2de67b6e90d567
niiho-katsumi <amanojaku.ktm@gmail.com>
2026-05-05 12:25:40 +0900

    記事詳細ページ仮作成

---
+++ src/components/pages/ArticleDetail/ArticleDetail.tsx
@@ -0,0 +1,10 @@
+import Layout from "../../Layout/Layout";
+import "../../../index.css";
+
+export default function ArticleDetail() {
+  return (
+    <>
+      <Layout>ArticleDetail Page</Layout>
+    </>
+  );
+}
