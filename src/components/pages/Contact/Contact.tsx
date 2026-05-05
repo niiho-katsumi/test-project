fb10188a846831d722d3b88a3208883c13811ad6
niiho-katsumi <amanojaku.ktm@gmail.com>
2026-05-05 12:25:11 +0900

    お問い合わせページ仮作成

---
+++ src/components/pages/Contact/Contact.tsx
@@ -0,0 +1,10 @@
+import Layout from "../../Layout/Layout";
+import "../../../index.css";
+
+export default function Contact() {
+  return (
+    <>
+      <Layout>Contact Page</Layout>
+    </>
+  );
+}
