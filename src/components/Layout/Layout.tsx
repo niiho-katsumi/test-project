d4d46109b7f795032b630364b75e6ccc5aa01b40
niiho-katsumi <amanojaku.ktm@gmail.com>
2026-05-05 09:46:17 +0900

    Layoutコンポーネント作成

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,15 @@
+import Header from "../organisms/Header/Header";
+import "../../index.css";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export default function Layout({ children }: Props) {
+  return (
+    <>
+      <Header />
+      <div className="my-10 mx-auto max-w-200">{children}</div>
+    </>
+  );
+}
