6080cc639142ea3e7fb4cb51003ded1336c35e0c
niiho-katsumi <amanojaku.ktm@gmail.com>
2026-05-05 12:26:15 +0900

    お問い合わせページ・記事詳細ページルーティング設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./components/pages/Home/Home";
 import "./index.css";
+import Contact from "./components/pages/Contact/Contact";
+import ArticleDetail from "./components/pages/ArticleDetail/ArticleDetail";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" Component={Home} />
+        <Route path="/contact" Component={Contact} />
+        <Route path="/posts/:id" Component={ArticleDetail} />
       </Routes>
     </BrowserRouter>
   );
