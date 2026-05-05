c8418aeab1f3841efa4b2440146fe1975cb556c2
niiho-katsumi <amanojaku.ktm@gmail.com>
2026-05-05 09:40:50 +0900

    Titleコンポーネント作成

---
+++ src/components/atoms/Title/Title.tsx
@@ -0,0 +1,9 @@
+import "../../../index.css";
+
+type Props = {
+  title: string;
+};
+
+export default function Title({ title }: Props) {
+  return <p className="text-[1.5rem] mt-2 mb-4 ">{title}</p>;
+}
