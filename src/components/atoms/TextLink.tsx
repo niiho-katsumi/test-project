d769deb316554e9ceb04d6ef592b55fa75942201
niiho-katsumi <amanojaku.ktm@gmail.com>
2026-05-05 17:03:43 +0900

    TextLinkに遷移先をpropsで渡せるように修正

--- src/components/atoms/TextLink.tsx
+++ src/components/atoms/TextLink.tsx
@@ -2,11 +2,12 @@ import { Link } from "react-router-dom";
 
 type Props = {
   text: string;
+  to: string;
 };
 
-export default function TextLink({ text }: Props) {
+export default function TextLink({ text, to }: Props) {
   return (
-    <Link className="text-white font-bold" to="/">
+    <Link className="text-white font-bold" to={to}>
       {text}
     </Link>
   );
--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -4,8 +4,8 @@ import TextLink from "../atoms/TextLink";
 export default function Header() {
   return (
     <header className="flex items-center justify-between p-6 bg-[#333]">
-      <TextLink text={"Blog"} />
-      <TextLink text={"お問い合わせ"} />
+      <TextLink text={"Blog"} to={"/"} />
+      <TextLink text={"お問い合わせ"} to={"/contact"} />
     </header>
   );
 }
