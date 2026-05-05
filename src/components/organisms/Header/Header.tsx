fefcc2f8b8c6fe33e2d69bd7306ae0e5d810a325
niiho-katsumi <amanojaku.ktm@gmail.com>
2026-05-05 12:50:56 +0900

    TextLinkをHeaderへ実装

--- src/components/organisms/Header/Header.tsx
+++ src/components/organisms/Header/Header.tsx
@@ -1,14 +1,11 @@
 import "../../../index.css";
+import TextLink from "../../atoms/TextLink/TextLink";
 
 export default function Header() {
   return (
     <header className="flex items-center justify-between p-6 bg-[#333]">
-      <a className="text-white font-bold" href="/">
-        Blog
-      </a>
-      <a className="text-white font-bold" href="/contact">
-        お問い合わせ
-      </a>
+      <TextLink text={"Blog"} />
+      <TextLink text={"お問い合わせ"} />
     </header>
   );
 }
