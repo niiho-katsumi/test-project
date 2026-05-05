04873679794011e43de8a5d2366605e0a4931c2e
niiho-katsumi <amanojaku.ktm@gmail.com>
2026-05-05 12:18:07 +0900

    Homeのルーティング設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./components/pages/Home/Home";
+import "./index.css";
 
 function App() {
-
   return (
-    <>    
-    test
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" Component={Home} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
