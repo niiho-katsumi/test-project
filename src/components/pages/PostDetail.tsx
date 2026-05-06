0bcac501d286e4dc2c443f051170b9fe9a1d7699
niiho-katsumi <amanojaku.ktm@gmail.com>
2026-05-06 22:48:24 +0900

    記事詳細情報をAPI経由で取得

--- src/components/pages/PostDetail.tsx
+++ src/components/pages/PostDetail.tsx
@@ -1,20 +1,31 @@
 import { useParams } from "react-router-dom";
 import Layout from "../../layouts/Layout";
 import PostDetailList from "../organisms/PostDetailList";
-import { posts } from "../../data/posts";
+import { useEffect, useState } from "react";
+import type { PostInfo } from "../../type";
 
 export default function PostDetail() {
+  const [post, setPost] = useState<PostInfo>();
+
   const { id } = useParams<{ id: string }>();
-  const filteredPost = posts.find((post) => post.id === Number(id));
-  if (!filteredPost) return <p>記事が見つかりません</p>;
+
+  useEffect(() => {
+    const fetcher = async () => {
+      const res = await fetch(
+        `https://1hmfpsvto6.execute-api.ap-northeast-1.amazonaws.com/dev/posts/${id}`,
+      );
+      const data = await res.json();
+      setPost(data.post);
+    };
+    fetcher();
+  }, []);
+
+  if (!post) return <p>記事が見つかりません</p>;
+
   return (
     <>
       <Layout>
-        <PostDetailList
-          post={filteredPost}
-          omission={false}
-          outline={false}
-        />
+        <PostDetailList post={post} omission={false} outline={false} />
       </Layout>
     </>
   );
