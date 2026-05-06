9d879de8bd021f0edd876092aa4f79e24a159e3a
niiho-katsumi <amanojaku.ktm@gmail.com>
2026-05-06 23:39:49 +0900

    記事一覧情報の取得中に読み込み中の文言は表示されるように修正

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -5,14 +5,17 @@ import type { PostInfo } from "../../type";
 
 export default function Home() {
   const [posts, setPosts] = useState<PostInfo[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
+    setIsLoading(true);
     const fetcher = async (): Promise<void> => {
       const res = await fetch(
         "https://1hmfpsvto6.execute-api.ap-northeast-1.amazonaws.com/dev/posts",
       );
       const data = await res.json();
       setPosts(data.posts);
+      setIsLoading(false);
     };
     fetcher();
   }, []);
@@ -20,7 +23,13 @@ export default function Home() {
   return (
     <>
       <Layout>
-        <PostList posts={posts} omission={true} outline={true} />
+        {isLoading ? (
+          <p>読み込み中...</p>
+        ) : !posts ? (
+          <p>記事が見つかりません</p>
+        ) : (
+          <PostList posts={posts} omission={true} outline={true} />
+        )}
       </Layout>
     </>
   );
