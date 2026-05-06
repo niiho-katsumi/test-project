f79fab412971fb1ef0f974fb3b76345fa4475a26
niiho-katsumi <amanojaku.ktm@gmail.com>
2026-05-06 11:55:55 +0900

    ArticleItemコンポーネント作成

---
+++ src/components/organisms/ArticleItem.tsx
@@ -0,0 +1,38 @@
+import type { ReactNode } from "react";
+import Content from "../atoms/Content";
+import DateText from "../atoms/DateText";
+import Title from "../atoms/Title";
+import TagList from "../molecules/TagList";
+
+type Props = {
+  createdAt: string;
+  categories: string[];
+  title: string;
+  content: ReactNode;
+  omission: boolean;
+  outline: boolean;
+};
+
+export default function ArticleItem({
+  createdAt,
+  categories,
+  title,
+  content,
+  omission,
+  outline,
+}: Props) {
+  return (
+    <>
+      <div
+        className={`p-4 mb-8 ${outline ? "border border-[#ccc] border-solid" : ""}`}
+      >
+        <div className="flex justify-between">
+          <DateText createdAt={createdAt} />
+          <TagList categories={categories} />
+        </div>
+        <Title title={title} />
+        <Content omission={omission}>{content}</Content>
+      </div>
+    </>
+  );
+}
