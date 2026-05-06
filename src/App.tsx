8adb52ea6a0b47717764e3f0df4eee10161e3c65
niiho-katsumi <amanojaku.ktm@gmail.com>
2026-05-06 23:58:38 +0900

    App.tsxにLayoutコンポーネント実装

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,18 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./components/pages/Home";
 import Contact from "./components/pages/Contact";
 import PostDetail from "./components/pages/PostDetail";
+import Layout from "./layouts/Layout";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" Component={Home} />
-        <Route path="/contact" Component={Contact} />
-        <Route path="/posts/:id" Component={PostDetail} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path="/" Component={Home} />
+          <Route path="/contact" Component={Contact} />
+          <Route path="/posts/:id" Component={PostDetail} />
+        </Routes>
+      </Layout>
     </BrowserRouter>
   );
 }
