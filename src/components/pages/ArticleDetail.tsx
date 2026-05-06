7ff4cb4cb04b68ca8f88fc5c19540e20271fa7a1
niiho-katsumi <amanojaku.ktm@gmail.com>
2026-05-06 12:04:51 +0900

    記事詳細の内容を取得する方法を実装

--- src/components/pages/ArticleDetail.tsx
+++ src/components/pages/ArticleDetail.tsx
@@ -1,9 +1,21 @@
+import { useParams } from "react-router-dom";
 import Layout from "../../layouts/Layout";
+import ArticleDetailList from "../organisms/ArticleDetailList";
+import { posts } from "../../data/posts";
 
 export default function ArticleDetail() {
+  const { id } = useParams<{ id: string }>();
+  const filteredArticle = posts.find((post) => post.id === Number(id));
+  if (!filteredArticle) return;
   return (
     <>
-      <Layout>ArticleDetail Page</Layout>
+      <Layout>
+        <ArticleDetailList
+          article={filteredArticle}
+          omission={false}
+          outline={false}
+        />
+      </Layout>
     </>
   );
 }
