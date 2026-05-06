3c119ac34d70e87e04cb9cbdd7fb9c77629236cd
niiho-katsumi <amanojaku.ktm@gmail.com>
2026-05-06 22:47:40 +0900

    記事一覧情報をAPI経由で取得

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,8 +1,22 @@
-import { posts } from "../../data/posts";
+import { useEffect, useState } from "react";
 import Layout from "../../layouts/Layout";
 import PostList from "../organisms/PostList";
+import type { PostInfo } from "../../type";
 
 export default function Home() {
+  const [posts, setPosts] = useState<PostInfo[]>([]);
+
+  useEffect(() => {
+    const fetcher = async (): Promise<void> => {
+      const res = await fetch(
+        "https://1hmfpsvto6.execute-api.ap-northeast-1.amazonaws.com/dev/posts",
+      );
+      const data = await res.json();
+      setPosts(data.posts);
+    };
+    fetcher();
+  }, []);
+
   return (
     <>
       <Layout>
