a312c191fbdc1e578c36dee1637df65ecebd81d5
niiho-katsumi <amanojaku.ktm@gmail.com>
2026-05-06 23:40:15 +0900

    記事詳細情報の取得中に読み込み中の文言は表示されるように修正

--- src/components/pages/PostDetail.tsx
+++ src/components/pages/PostDetail.tsx
@@ -6,26 +6,33 @@ import type { PostInfo } from "../../type";
 
 export default function PostDetail() {
   const [post, setPost] = useState<PostInfo>();
+  const [isLoading, setIsLoading] = useState(false);
 
   const { id } = useParams<{ id: string }>();
 
   useEffect(() => {
+    setIsLoading(true);
     const fetcher = async (): Promise<void> => {
       const res = await fetch(
         `https://1hmfpsvto6.execute-api.ap-northeast-1.amazonaws.com/dev/posts/${id}`,
       );
       const data = await res.json();
       setPost(data.post);
+      setIsLoading(false);
     };
     fetcher();
   }, []);
 
-  if (!post) return <p>記事が見つかりません</p>;
-
   return (
     <>
       <Layout>
-        <PostDetailList post={post} omission={false} outline={false} />
+        {isLoading ? (
+          <p>読み込み中...</p>
+        ) : !post ? (
+          <p>記事が見つかりません</p>
+        ) : (
+          <PostDetailList post={post} omission={false} outline={false} />
+        )}
       </Layout>
     </>
   );
