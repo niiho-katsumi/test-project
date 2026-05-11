2a2f1dce3a36a92a05f9a1607cf287349cd7b101
niiho-katsumi <amanojaku.ktm@gmail.com>
2026-05-12 05:18:56 +0900

    不要なimport文削除

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import Layout from "../../layouts/Layout";
 import PostList from "../organisms/PostList";
 import type { PostInfo } from "../../type";
 
