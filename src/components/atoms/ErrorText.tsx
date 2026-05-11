286ccecca3ca925ed01763cf82aba9aca5bc14e8
niiho-katsumi <amanojaku.ktm@gmail.com>
2026-05-12 05:17:03 +0900

    ErrorTextコンポーネント作成

---
+++ src/components/atoms/ErrorText.tsx
@@ -0,0 +1,10 @@
+import type { FieldError } from "react-hook-form";
+
+type Porps = {
+  errorText?: string;
+  error?: FieldError;
+};
+
+export default function ErrorText({ errorText, error }: Porps) {
+  return <>{error && <span className="text-red-500">{errorText}</span>}</>;
+}
