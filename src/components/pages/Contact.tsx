18a10c86e6d6d8f1d3e9a4a5afd5345f24e61398
niiho-katsumi <amanojaku.ktm@gmail.com>
2026-05-12 05:18:19 +0900

    Contactページ実装

--- src/components/pages/Contact.tsx
+++ src/components/pages/Contact.tsx
@@ -1,7 +1,28 @@
+import { type contactSchemaType } from "../../schemas/contact.schema";
+import ContactForm from "../organisms/ContactForm";
+
 export default function Contact() {
+  const onSubmit = async (data: contactSchemaType) => {
+    try {
+      const res = await fetch(
+        "https://1hmfpsvto6.execute-api.ap-northeast-1.amazonaws.com/dev/contacts",
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(data),
+        },
+      );
+
+      alert("送信しました");
+      if (!res.ok) throw new Error(`httpエラー:${res.status}`);
+    } catch (error) {
+      throw error;
+    }
+  };
+
   return (
     <>
-      <>Contact Page</>
+      <ContactForm onSubmit={onSubmit} />
     </>
   );
 }
